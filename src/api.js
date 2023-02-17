a615d75dabfa711271fd25dde6125b87c5af7c28
Narek Hovhannisyan <hovnarek@gmail.com>
2023-02-17 14:17:46 +0400

    ended

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ccf2b55972mshc7a8e337ff1ea1bp17ec04jsnaf05723d93f0",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8308363a9bf9ff9b87ee258728bc4509";
